ef57395aaa96f34846a833e80c28b379c7e91e17
MaryBlessing Umeh <umeh.mary@appmartgroup.com>
2020-09-20 08:35:54 +0100

    Added SEO and some setups

--- src/main.js
+++ src/main.js
@@ -1,26 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
-import axios from 'axios'
-import Vuelidate from  'vuelidate';
-import VueToastr from "vue-toastr";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import moment from 'moment';
-
 import "@/assets/css/tailwind.css";
+import Meta from "vue-meta";
 
-Vue.prototype.$http = axios;
+Vue.use(Meta);
+
 Vue.prototype.$moment = moment;
 
-Vue.use(Vuelidate)
-Vue.use(VueToastr, {});
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,6 +33,65 @@ export default {
     HostelLife,
     News,
     Footer
+  },
+  metaInfo: {
+    // Children can override the title.
+    title: "Swizel Technologies",
+    // Result: My Page Title ← My Site
+    // If a child changes the title to "My Other Page Title",
+    // it will become: My Other Page Title ← My Site
+    titleTemplate: "%s ← Swizel",
+    // Define meta tags here.
+    meta: [
+      { property: "og:site_name", content: "Swizel" },
+      { property: "og:title", content: "Swizel" },
+      { property: "og:url", content: "https://www.swizel.co" },
+      {
+        property: "og:image",
+        content: "https://swizel.co/img/header-logo.091c72f8.svg"
+      },
+      {
+        property: "og:type",
+        content: "website"
+      },
+      {
+        property: "og:description",
+        content:
+          "Swizel is an IT solution company with primary emphasis on harnessing web techologies to deliver solutions which aim at improving way of life and ease of doing business across all sectors of human life"
+      },
+      {
+        property: "og:keywords",
+        content:
+          "Website design, Mobile app, Software Development, It support, Data Analysis and Management, Digital Marketing, Networking, Consultancy"
+      },
+      { itemprop: "name", content: "Swizel" },
+      { itemprop: "url", content: "https://swizel.co" },
+      {
+        itemprop: "description",
+        content:
+          "Swizel is an IT solution company with primary emphasis on harnessing web techologies to deliver solutions which aim at improving way of life and ease of doing business across all sectors of human life"
+      },
+      { name: "twitter:title", content: "Swizel" },
+      { name: "twitter:url", content: "https://swizel.co" },
+      { name: "twitter:card", content: "summary_large_image" },
+      {
+        name: "twitter:image:src",
+        content: "https://swizel.co/img/header-logo.091c72f8.svg"
+      },
+      { name: "twitter:site", content: "@swizelhq" },
+      { name: "twitter:creator", content: "@swizelhg" },
+      {
+        name: "twitter:description",
+        content:
+          "Swizel is an IT solution company with primary emphasis on harnessing web techologies to deliver solutions which aim at improving way of life and ease of doing business across all sectors of human life"
+      },
+      {
+        name: "description",
+        content:
+          "Swizel is an IT solution company with primary emphasis on harnessing web techologies to deliver solutions which aim at improving way of life and ease of doing business across all sectors of human life"
+      }
+    ],
+    link: [{ rel: "canonical", href: "https://www.swizel.co" }]
   }
 }
 </script>
