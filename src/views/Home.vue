829001e79ca208936a1da9e5800d3f66136f30a2
MaryBlessing Umeh <umeh.mary@appmartgroup.com>
2020-09-20 09:01:42 +0100

    A new fix for Netlify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,14 +8,14 @@
 
 <script>
 // @ is an alias to /src
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
+// import Header from '@/components/Header.vue';
+// import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'Home',
   components: {
-    Header,
-    Footer
+    // Header,
+    // Footer
   },
   metaInfo: {
     // Children can override the title.
